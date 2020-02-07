b36dcfed5c8f73bb2d5b7d1065d275afddb6317b
i963 <60781733+i963@users.noreply.github.com>
2020-02-07 14:48:01 +0200

    Add files via upload

---
+++ random.java
@@ -0,0 +1,8 @@
+class RandomInt {
+    public static void main(String[] args) {
+        Random random = new Random();
+        for (int i = 0; i < 30; i++) {
+            System.out.print(random.nextInt(10) + " ");
+        }
+    }
+}
